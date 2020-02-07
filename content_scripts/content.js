50c8806bd4d2d5be7408313c4a9bc57324f9f5d4
mattfan00 <kungfumo@gmail.com>
2020-02-07 15:45:32 -0500

    added new stuff

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -5,20 +5,44 @@
    * it will do nothing next time.
    */
 
+  let myport = browser.runtime.connect({name: "port from content script"})
+
   var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
   var headerInfo = []
-  console.log(headers)
-  headers.forEach((header) => {
+  headers.forEach((header, i) => {
+    header.id = "h-" + i
+    
     headerInfo.push({
       textContent: header.textContent,
       headerType: header.tagName
     })
   })
-
   console.log(headerInfo)
+  // document.getElementById("h-1").scrollIntoView()
 
-  browser.runtime.sendMessage({
+  myport.postMessage({
     headers: headerInfo
   })
+
+  myPort.onMessage.addListener(function(m) {
+    document.getElementById("h-1").scrollIntoView()
+    document.getElementById("h-1").textContent = m.greeting
+  });
+
+
+  // browser.runtime.onMessage.addListener((message) => {
+  //   console.log(hi)
+  // });
+  // browser.runtime.onConnect.addListener((p) => {
+  //   extensionport = p
+  //   extensionport.onMessage.addListener(function(message) {
+  //     console.log("In content script, received message from extension");
+  //     console.log(message.greeting);
+  //   });
+  // });
+
+  // browser.runtime.sendMessage({
+  //   headers: headerInfo
+  // })
 
 })();
--- popup/index.js
+++ popup/index.js
@@ -1,10 +1,53 @@
-browser.runtime.onMessage.addListener((message) => { // gets the information from the content.js and displays it in the popup
-  message.headers.forEach((header) => {
-    var temp = document.createElement(header.headerType)
-    temp.textContent = header.textContent
-    document.getElementById("contents").appendChild(temp)
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+    headerId = e.target.id
+    document.getElementById("test").textContent = headerId
+    // browser.tabs.sendMessage(tabs[0].id, {
+    //   headerId: headerId 
+    // })
+    
+  })
+}
+
+// browser.runtime.onMessage.addListener((message) => { // gets the information from the content.js and displays it in the popup
+//   message.headers.forEach((header, i) => {
+//     var temp = document.createElement(header.headerType)
+//     temp.textContent = header.textContent
+//     temp.id = "h-" + i
+//     document.getElementById("contents").appendChild(temp)
+//   })
+
+// });
+
+let portFromCS  
+
+function connected(p) {
+  portFromCS = p;
+  
+  portFromCS.onMessage.addListener(function(m) {
+    m.headers.forEach((header, i) => {
+      var temp = document.createElement(header.headerType)
+      temp.textContent = header.textContent
+      temp.id = "h-" + i
+      document.getElementById("contents").appendChild(temp)
+    })
+  });
+
+  document.addEventListener("click", (e) => {
+    headerId = e.target.id
+    document.getElementById("test").textContent = headerId
+    // browser.tabs.sendMessage(tabs[0].id, {
+    //   headerId: headerId 
+    // })
+    portFromCS.postMessage({
+      greeting: "this works!"
+    })
+    
   })
 
-});
+  
+}
+
+browser.runtime.onConnect.addListener(connected);
 
 browser.tabs.executeScript({file: "/content_scripts/content.js"})
