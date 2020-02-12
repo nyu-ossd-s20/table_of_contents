153a3d5dbf72d8b2efc45f43b9c560fe0fc0fdc0
princecarter <pc2415@nyu.edu>
2020-02-12 13:13:56 -0500

    cleaned up html

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,6 +1,6 @@
 (function() {
   let myport = browser.runtime.connect({name: "port from content script"})
-
+  console.log(myport)
   var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
   var headerInfo = []
   headers.forEach((header, i) => {
@@ -16,6 +16,7 @@
   })
 
   myport.onMessage.addListener(function(m) {
+
     document.getElementById(m.headerID).scrollIntoView()
   });
 })();
